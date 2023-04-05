37d63c5375cb9db9d225bfb59c2f1eb5b3a64f49
Saltgurkan24 <129761986+Saltgurkan24@users.noreply.github.com>
2023-04-05 11:34:09 +0200

    Testkörning 1

--- lego_arm/main.py
+++ lego_arm/main.py
@@ -15,18 +15,37 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 # Create your objects here.
 # Definitions
 ev3 = EV3Brick()
-turning_motor = Motor(Port.C)
-arm_motor = Motor(Port.B)
-claw_motor = Motor(Port.A)
+base_motor = Motor(Port.C)
+elbow_motor = Motor(Port.B)
+gripper_motor = Motor(Port.A)
 color_sensor = ColorSensor(Port.S2)
+base_switch = TouchSensor(Port.S1)
 
 ev3.speaker.beep()
 
-turning_motor.run_angle(70, -180, then=Stop.COAST)
+wait(10)
 
-arm_motor.run_until_stalled(-100, then=Stop.HOLD, duty_limit=20)
+base_motor.run(-60)
+while not base_switch.pressed():
+    wait(10)
+base_motor.reset_angle(0)
+base_motor.hold()
 
-turning_motor.run_target(70,0, then=Stop.COAST)
+elbow_motor.run_time(-30, 1000)
+elbow_motor.run(15)
+wait(10)
+elbow_motor.reset_angle(0)
+elbow_motor.hold()
+
+gripper_motor.run_until_stalled(200, then=Stop.COAST, duty_limit=50)
+gripper_motor.reset_angle(0)
+gripper_motor.run_target(200, -90)
+
+#turning_motor.run_angle(70, -180, then=Stop.COAST)
+
+#arm_motor.run_until_stalled(-100, then=Stop.HOLD, duty_limit=20)
+
+#turning_motor.run_target(70,0, then=Stop.COAST)
 
 # while True:
 #    print(turning_motor.angle())
