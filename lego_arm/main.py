2be908e0f7d38020733201f7816eae2556ce5356
Ali Reza Hussaini <106796294+ahussaini069@users.noreply.github.com>
2023-04-17 09:30:42 +0200

    test 2

--- lego_arm/main.py
+++ lego_arm/main.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+
+
+
 ev3 = EV3Brick()
 turning_motor = Motor(Port.C)
 arm_motor = Motor(Port.B)
